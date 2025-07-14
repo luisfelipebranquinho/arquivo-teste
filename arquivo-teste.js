35446dff4cc184e3eb64f2439540ca9ab68587b1
Luis Felipe Branquinho <branquinho1034@gmail.com>
2025-07-13 23:27:00 -0300

    Refazendo

--- arquivo-teste.js
+++ arquivo-teste.js
@@ -6,17 +6,4 @@ function quadrado(){
     return resultado
 }
 
-quadrado();
-
-<<<<<<< HEAD
-console.log(final);
-
-// alteracao teste 1
-// commit 1
-// adicionado oque eu esqueci
-let vadr = 1110
-let vadrr = 1111
-let mais = 10
-=======
-console.log(final);
->>>>>>> parent of 8b3996a (segundo commit)
+// Refazendo
